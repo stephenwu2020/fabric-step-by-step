cbf9e3b1b1a4250aca8c82d1662ef0720ab98423
wusiyu <stephenwu2019@163.com>
2020-03-06 19:07:31 +0800

    rm unused

--- download-binary.sh
+++ download-binary.sh
@@ -15,55 +15,7 @@ ARCH=$(echo "$(uname -s|tr '[:upper:]' '[:lower:]'|sed 's/mingw64_nt.*/windows/'
 MARCH=$(uname -m)
 
 printHelp() {
-    echo "Usage: bootstrap.sh [version [ca_version [thirdparty_version]]] [options]"
-    echo
-    echo "options:"
-    echo "-h : this help"
-    echo "-d : bypass docker image download"
-    echo "-s : bypass fabric-samples repo clone"
-    echo "-b : bypass download of platform-specific binaries"
-    echo
-    echo "e.g. bootstrap.sh 2.0.0 1.4.6 0.4.18 -s"
-    echo "would download docker images and binaries for Fabric v2.0.0 and Fabric CA v1.4.6"
-}
-
-# dockerPull() pulls docker images from fabric and chaincode repositories
-# note, if a docker image doesn't exist for a requested release, it will simply
-# be skipped, since this script doesn't terminate upon errors.
-
-dockerPull() {
-    #three_digit_image_tag is passed in, e.g. "1.4.6"
-    three_digit_image_tag=$1
-    shift
-    #two_digit_image_tag is derived, e.g. "1.4", especially useful as a local tag for two digit references to most recent baseos, ccenv, javaenv, nodeenv patch releases
-    two_digit_image_tag=$(echo $three_digit_image_tag | cut -d'.' -f1,2)
-    while [[ $# -gt 0 ]]
-    do
-        image_name="$1"
-        echo "====> hyperledger/fabric-$image_name:$three_digit_image_tag"
-        docker pull "hyperledger/fabric-$image_name:$three_digit_image_tag"
-        docker tag "hyperledger/fabric-$image_name:$three_digit_image_tag" "hyperledger/fabric-$image_name"
-        docker tag "hyperledger/fabric-$image_name:$three_digit_image_tag" "hyperledger/fabric-$image_name:$two_digit_image_tag"
-        shift
-    done
-}
-
-cloneSamplesRepo() {
-    # clone (if needed) hyperledger/fabric-samples and checkout corresponding
-    # version to the binaries and docker images to be downloaded
-    if [ -d first-network ]; then
-        # if we are in the fabric-samples repo, checkout corresponding version
-        echo "===> Checking out v${VERSION} of hyperledger/fabric-samples"
-        git checkout v${VERSION}
-    elif [ -d fabric-samples ]; then
-        # if fabric-samples repo already cloned and in current directory,
-        # cd fabric-samples and checkout corresponding version
-        echo "===> Checking out v${VERSION} of hyperledger/fabric-samples"
-        cd fabric-samples && git checkout v${VERSION}
-    else
-        echo "===> Cloning hyperledger/fabric-samples repo and checkout v${VERSION}"
-        git clone -b master https://github.com/hyperledger/fabric-samples.git && cd fabric-samples && git checkout v${VERSION}
-    fi
+    echo "Usage: download-binary.sh"
 }
 
 # This will download the .tar.gz
@@ -100,42 +52,6 @@ pullBinaries() {
     fi
 }
 
-pullDockerImages() {
-    command -v docker >& /dev/null
-    NODOCKER=$?
-    if [ "${NODOCKER}" == 0 ]; then
-        FABRIC_IMAGES=(peer orderer ccenv tools)
-        case "$VERSION" in
-        1.*)
-            FABRIC_IMAGES+=(javaenv)
-            shift
-            ;;
-        2.*)
-            FABRIC_IMAGES+=(nodeenv baseos javaenv)
-            shift
-            ;;
-        esac
-        echo "FABRIC_IMAGES:" "${FABRIC_IMAGES[@]}"
-        echo "===> Pulling fabric Images"
-        dockerPull "${FABRIC_TAG}" "${FABRIC_IMAGES[@]}"
-        echo "===> Pulling fabric ca Image"
-        CA_IMAGE=(ca)
-        dockerPull "${CA_TAG}" "${CA_IMAGE[@]}"
-        echo "===> Pulling thirdparty docker images"
-        THIRDPARTY_IMAGES=(zookeeper kafka couchdb)
-        dockerPull "${THIRDPARTY_TAG}" "${THIRDPARTY_IMAGES[@]}"
-        echo
-        echo "===> List out hyperledger docker images"
-        docker images | grep hyperledger
-    else
-        echo "========================================================="
-        echo "Docker not installed, bypassing download of Fabric images"
-        echo "========================================================="
-    fi
-}
-
-DOCKER=false
-SAMPLES=false
 BINARIES=true
 
 # Parse commandline args pull out
@@ -181,21 +97,9 @@ while getopts "h?dsb" opt; do
     esac
 done
 
-if [ "$SAMPLES" == "true" ]; then
-    echo
-    echo "Clone hyperledger/fabric-samples repo"
-    echo
-    cloneSamplesRepo
-fi
 if [ "$BINARIES" == "true" ]; then
     echo
     echo "Pull Hyperledger Fabric binaries"
     echo
     pullBinaries
-fi
-if [ "$DOCKER" == "true" ]; then
-    echo
-    echo "Pull Hyperledger Fabric docker images"
-    echo
-    pullDockerImages
 fi
