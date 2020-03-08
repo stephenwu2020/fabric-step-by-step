c0652e94861dda07c7004a51dc20bd81c76fd16f
stephenwu <stephenwu2020@outlook.com>
2020-03-08 15:00:26 +0800

    commit check

--- 06-smart-contract-chaincode/scripts/chaincode.sh
+++ 06-smart-contract-chaincode/scripts/chaincode.sh
@@ -2,6 +2,7 @@
 
 MODE=$1
 CHANNEL_NAME="c1"
+CHAINCODE_NAME="mycc"
 CAFILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/organizations/ordererOrganizations/demo.com/orderers/o4.demo.com/msp/tlscacerts/tlsca.demo.com-cert.pem
 TAG="2.0.0"
 
@@ -24,15 +25,16 @@ function help(){
   echo "  - package"
   echo "  - install"
   echo "  - approve"
-  echo "  - check"
+  echo "  - beforeCommit"
   echo "  - commit"
+  echo "  - afterCommit"
   echo "  - invoke"
   echo "  - query"
   echo "  - start"
 }
 
 function package(){
-  if [ -f "pkg/mycc.tar.gz" ]; then
+  if [ -f "pkg/${CHAINCODE_NAME}.tar.gz" ]; then
     echo "pkg already exist"
   else
     echo "fetch go dependency"
@@ -40,12 +42,12 @@ function package(){
     go mod vendor
 
     echo "package chaincode"
-    peer lifecycle chaincode package mycc.tar.gz \
+    peer lifecycle chaincode package ${CHAINCODE_NAME}.tar.gz \
       --path github.com/hyperledger/fabric-samples/chaincode/abstore/go/ \
       --lang golang \
       --label fabcar_1
 
-    cp mycc.tar.gz /opt/gopath/src/github.com/hyperledger/fabric/peer/pkg
+    cp ${CHAINCODE_NAME}.tar.gz /opt/gopath/src/github.com/hyperledger/fabric/peer/pkg
   fi
 }
 
@@ -57,14 +59,14 @@ function install(){
   CORE_PEER_ADDRESS=${R1ADDR}
   CORE_PEER_LOCALMSPID=${R1MSPID}
   CORE_PEER_TLS_ROOTCERT_FILE=${R1CRT}
-  peer lifecycle chaincode install /opt/gopath/src/github.com/hyperledger/fabric/peer/pkg/mycc.tar.gz
+  peer lifecycle chaincode install /opt/gopath/src/github.com/hyperledger/fabric/peer/pkg/${CHAINCODE_NAME}.tar.gz
 
   # install on peer of r2
   CORE_PEER_MSPCONFIGPATH=${R2MSP}
   CORE_PEER_ADDRESS=${R2ADDR}
   CORE_PEER_LOCALMSPID=${R2MSPID}
   CORE_PEER_TLS_ROOTCERT_FILE=${R2CRT}
-  peer lifecycle chaincode install /opt/gopath/src/github.com/hyperledger/fabric/peer/pkg/mycc.tar.gz
+  peer lifecycle chaincode install /opt/gopath/src/github.com/hyperledger/fabric/peer/pkg/${CHAINCODE_NAME}.tar.gz
 
 }
 
@@ -82,7 +84,7 @@ function approve(){
   CORE_PEER_TLS_ROOTCERT_FILE=${R1CRT}
   peer lifecycle chaincode approveformyorg \
     --channelID $CHANNEL_NAME \
-    --name mycc \
+    --name ${CHAINCODE_NAME} \
     --version 1.0 \
     --init-required \
     --package-id $PACKAGE_ID \
@@ -98,7 +100,7 @@ function approve(){
   CORE_PEER_TLS_ROOTCERT_FILE=${R2CRT}
   peer lifecycle chaincode approveformyorg \
     --channelID $CHANNEL_NAME \
-    --name mycc \
+    --name ${CHAINCODE_NAME} \
     --version 1.0 \
     --init-required \
     --package-id $PACKAGE_ID \
@@ -107,11 +109,22 @@ function approve(){
     --cafile $CAFILE
 }
 
+function beforeCommit(){
+  # checkcommitreadiness
+  peer lifecycle chaincode checkcommitreadiness \
+    --channelID $CHANNEL_NAME \
+    --name ${CHAINCODE_NAME} \
+    --version 1.0 \
+    --sequence 1 \
+    --init-required \
+    --output json
+}
+
 function commit(){
   peer lifecycle chaincode commit \
     -o o4.demo.com:7050 \
     --channelID $CHANNEL_NAME \
-    --name mycc \
+    --name ${CHAINCODE_NAME} \
     --version 1.0 \
     --sequence 1 \
     --init-required \
@@ -123,15 +136,8 @@ function commit(){
     --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/organizations/peerOrganizations/r2.demo.com/peers/peer0.r2.demo.com/tls/ca.crt
 }
 
-function checkcommitreadiness(){
-  # checkcommitreadiness
-  peer lifecycle chaincode checkcommitreadiness \
-    --channelID $CHANNEL_NAME \
-    --name mycc \
-    --version 1.0 \
-    --sequence 1 \
-    --init-required \
-    --output json
+function afterCommit(){
+  peer lifecycle chaincode querycommitted --channelID ${CHANNEL_NAME} --name ${CHAINCODE_NAME}
 }
 
 function invoke(){
@@ -141,7 +147,7 @@ function invoke(){
     --tls \
     --cafile $CAFILE \
     -C $CHANNEL_NAME \
-    -n mycc \
+    -n ${CHAINCODE_NAME} \
     --peerAddresses peer0.r1.demo.com:7051 \
     --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/organizations/peerOrganizations/r1.demo.com/peers/peer0.r1.demo.com/tls/ca.crt \
     --peerAddresses peer0.r2.demo.com:8051 \
@@ -151,11 +157,7 @@ function invoke(){
 }
 
 function query(){
-  peer chaincode query -C $CHANNEL_NAME -n mycc -c '{"Args":["query","a"]}'
-}
-
-function debug(){
-  echo "peer chaincode invoke -o o4.demo.com:7050 --isInit -C c1 -n mycc -c '{"Args":["Init","a","100","b","100"]}' --waitForEvent --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/organizations/ordererOrganizations/demo.com/orderers/o4.demo.com/msp/tlscacerts/tlsca.demo.com-cert.pem"
+  peer chaincode query -C $CHANNEL_NAME -n ${CHAINCODE_NAME} -c '{"Args":["query","a"]}'
 }
 
 if [ "$MODE" == "package" ]; then
@@ -164,10 +166,12 @@ elif [ "$MODE" == "install" ]; then
   install
 elif [ "$MODE" == "approve" ]; then
   approve
+elif [ "$MODE" == "beforeCommit" ]; then
+  beforeCommit
 elif [ "$MODE" == "commit" ]; then
   commit
-elif [ "$MODE" == "check" ]; then
-  checkcommitreadiness
+elif [ "$MODE" == "afterCommit" ]; then
+  afterCommit
 elif [ "$MODE" == "invoke" ]; then
   invoke
 elif [ "$MODE" == "debug" ]; then
@@ -178,8 +182,9 @@ elif [ "$MODE" == "start" ]; then
   package
   install
   approve
-  checkcommitreadiness
+  beforeCommit
   commit
+  afterCommit
   invoke
   #query
 else        
