2cc5e46b7141f9ac8f6798f2fa2f1cfd2b78be78
stephenwu <stephenwu2020@protonmail.com>
2020-08-07 16:31:15 +0800

    network 4

--- 04-creating-a-channel/network.sh
+++ 04-creating-a-channel/network.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 MODE=$1
+CRYPTOGEN=../bin/cryptogen
+CONFIGTXGEN=../bin/configtxgen
 CHANNEL_NAME="c1"
-TAG="2.0.0"
 FABRIC_CFG_PATH=$PWD
 CAFILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/demo.com/orderers/o4.demo.com/msp/tlscacerts/tlsca.demo.com-cert.pem
 
@@ -19,27 +20,34 @@ function help(){
   echo "  - clear"
 }
 
-if [ "$MODE" == "crypto" ]; then
-  cryptogen generate --config=./crypto-config.yaml --output="organizations"
-elif [ "$MODE" == "genesis" ]; then
-  configtxgen -profile NC4 -channelID ordererchannel -outputBlock ./system-genesis-block/genesis.block
-elif [ "$MODE" == "channeltx" ]; then
-  configtxgen -profile CC1 -outputCreateChannelTx ./channel-artifacts/${CHANNEL_NAME}.tx -channelID $CHANNEL_NAME
-elif [ "$MODE" == "channel" ]; then
-  docker exec cli peer channel create \
+case "$MODE" in
+  "crypto")
+    ${CRYPTOGEN} generate --config=./crypto-config.yaml --output="organizations"
+    ;;
+  "genesis")
+    ${CONFIGTXGEN} -profile NC4 -channelID ordererchannel -outputBlock ./system-genesis-block/genesis.block
+    ;;
+  "channeltx")
+    ${CONFIGTXGEN} -profile CC1 -outputCreateChannelTx ./channel-artifacts/${CHANNEL_NAME}.tx -channelID $CHANNEL_NAME
+    ;;
+  "channel")
+    docker exec cli peer channel create \
     -o o4.demo.com:7050 \
     -c $CHANNEL_NAME \
     -f /opt/gopath/src/github.com/hyperledger/fabric/peer/channel-artifacts/${CHANNEL_NAME}.tx \
     --tls true \
     --cafile $CAFILE
-    
-elif [ "$MODE" == "up" ]; then
-  IMAGE_TAG=$TAG docker-compose up -d
-elif [ "$MODE" == "down" ]; then
-  IMAGE_TAG=$TAG docker-compose down
-elif [ "$MODE" == "clear" ]; then
-  rm -rf organizations system-genesis-block channel-artifacts
-else        
-  help
-  exit 1
-fi
+    ;;
+  "up")
+    docker-compose up -d
+    ;;
+  "down")
+    docker-compose down
+    ;;
+  "clear")
+    rm -rf organizations system-genesis-block channel-artifacts
+    ;;
+  *)
+    help
+    exit 1
+esac
