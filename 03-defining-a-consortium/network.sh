34c6819fe8cba5a249300dfaaad766f659507859
stephenwu <stephenwu2020@protonmail.com>
2020-08-07 16:25:14 +0800

    network 3

--- 03-defining-a-consortium/network.sh
+++ 03-defining-a-consortium/network.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 MODE=$1
-TAG="2.0.0"
+CRYPTOGEN=../bin/cryptogen
+CONFIGTXGEN=../bin/configtxgen
 
 function help(){
   echo "Usage: "
@@ -14,17 +15,23 @@ function help(){
   echo "  - clear"
 }
 
-if [ "$MODE" == "crypto" ]; then
-  cryptogen generate --config=./crypto-config.yaml --output="organizations"
-elif [ "$MODE" == "genesis" ]; then
-  configtxgen -profile NC4 -channelID ordererchannel -outputBlock ./system-genesis-block/genesis.block
-elif [ "$MODE" == "up" ]; then
-  IMAGE_TAG=$TAG docker-compose up -d
-elif [ "$MODE" == "down" ]; then
-  IMAGE_TAG=$TAG docker-compose down
-elif [ "$MODE" == "clear" ]; then
-  rm -rf organizations system-genesis-block
-else        
-  help
-  exit 1
-fi
+case "$MODE" in
+  "crypto")
+    ${CRYPTOGEN} generate --config=./crypto-config.yaml --output="organizations"
+    ;;
+  "genesis")
+    ${CONFIGTXGEN} -profile NC4 -channelID ordererchannel -outputBlock ./system-genesis-block/genesis.block
+    ;;
+  "up")
+    docker-compose up -d
+    ;;
+  "down")
+    docker-compose down
+    ;;
+  "clear")
+    rm -rf organizations system-genesis-block
+    ;;
+  *)
+    help
+    exit 1
+esac
